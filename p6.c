8cd8dc263f2c7b6651b3619b175c9645b10740cb
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:17:58 +0530

    defined a function to replace main function

--- p6.c
+++ p6.c
@@ -1,1 +1,8 @@
-  
+//Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+//Write a C program to find largest element in an array
+#include <stdio.h>
+#define prog_without_main main //to run a program without using main()
+int prog_without_main()
+{
+
+} 
