0382b43bb07e31b9532754dad8726b4cc5418200
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:00:03 +0530

    input feature added

---
+++ p2.c
@@ -0,0 +1,13 @@
+//Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+//Write a C program to check if a given number is Fibonacci number
+
+#include <stdio.h>
+int main()
+{
+    int n, a=0, b=1, c, i;//declaration
+    //Taking input from user
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+     
+}
