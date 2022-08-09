a0dc91ee007b300781a0377ba4de6067d432e083
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:23:00 +0530

    initial commit

---
+++ p7.c
@@ -0,0 +1,14 @@
+//Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+//Write a menu driven program to perform the following operations in an ARRAY to 1.Insert 2.Delete 3.Traverse 4.Search
+#include <stdio.h>
+#define size 5 //setting stack size
+
+int array[size], top=-1; //declaring array, top and functions
+void insert();
+void delete();
+void traverse();
+void search();
+
+int main()
+{
+}
