41f25fb2662b810d253ddddbb09b5d0d17e6ea8a
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:28:46 +0530

    initial commit

---
+++ p9.c
@@ -0,0 +1,9 @@
+//Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+//Write a function that compares two arrays and returns 1 if they are identical and 0 otherwise
+#include <stdio.h>
+#include <string.h>
+#define size 100 //setting array size to 100
+
+char arr1[size], arr2[size]; //declaring 2 arrays
+
+void compare(); //declaring compare function
