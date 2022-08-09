a274ef4cd15ee60d71999743eb4f743c25ffce05
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:09:41 +0530

    input feature added

---
+++ p3.c
@@ -0,0 +1,21 @@
+//Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+//Write a C program to generate Pascal Triangle using array
+#include <stdio.h>
+int main()
+{
+    int pt[100][100], i, j, n, s; //declaration
+    //Taking input from user
+    printf("\nEnter the number of lines to be printed: ");
+    scanf("%d", &n);
+
+    
+}
+
+/*
+|j=0|
+  1 
+  1     1
+  1  	2 	  1                        
+  1     3 	  3 	1
+  1     4	  6     4	  1
+*/
