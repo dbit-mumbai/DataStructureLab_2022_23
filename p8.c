1bde2fc1aede708291834caaf12c1eb95c5d5a4d
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:27:08 +0530

    insert, display and update functions added

--- p8.c
+++ p8.c
@@ -45,3 +45,81 @@ int main()
     }
     
 }
+
+void input()
+{
+    int h, m, s;
+    printf("Enter hour minute second separated by spaces : ");
+    scanf("%d %d %d", &h, &m, &s);
+    
+    while (s>=60) //to have seconds value under 60
+    {
+        s = s-60;
+        m++; //increments minutes by 1 
+    }
+    t.second=s; //saves the seconds value
+    
+    while (m>=60) //to have minutes value under 60
+    {
+        m = m-60; 
+        h++; //increments hours by 1 
+    }
+    t.minute=m; //saves the minutes value
+    
+    while (h>=24) //to have hours value under 24
+    {
+        h = h-24;
+    }
+    t.hour=h; //saves the hours value
+    count=1;
+
+}
+
+void display()
+{
+    if (count==0) //checks if time was input before display function was called
+    {
+        printf("Please input time first\n");
+    }
+    else
+    {
+        printf("%d %d %d\n", t.hour, t.minute, t.second); //displays time
+    }
+    
+}
+
+void update()
+{
+    if (count==0) //checks if time was input before display function was called
+    {
+        printf("Please input time first\n");
+    }
+    else
+    {
+        int h, m, s;
+        
+        h=t.hour, m=t.minute, s=t.second+1; //increments seconds value by 1
+        
+        while (s>=60) //to have seconds value under 60
+        {
+            s = s-60;
+            m++; //increments minutes by 1 
+        }
+        t.second=s; //saves the seconds value
+    
+        while (m>=60) //to have minutes value under 60
+        {
+            m = m-60; 
+            h++; //increments hours by 1 
+        }
+        t.minute=m; //saves the minutes value
+    
+        while (h>=24) //to have hours value under 24
+        {
+            h = h-24;
+        }
+        t.hour=h; //saves the hours value
+        count=1;
+    }
+
+}
