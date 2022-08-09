564b2691ddc370d15bd67fa70f593fc7e4638b7a
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:25:47 +0530

    initial commit

---
+++ p8.c
@@ -0,0 +1,20 @@
+/*  Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+    Define a structure data type called time_struct containing 3 members int hour, int minute and int second.
+    1.Input time    2.Display time  3.Update time(increment time by 1 second)
+*/
+#include <stdio.h>
+
+void input(); //declaring functions
+void display();
+void update();
+
+struct time_struct //declaring and defining structure
+{
+    int hour, minute, second;
+}t;
+
+int count=0;
+
+int main()
+{
+}
