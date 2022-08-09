f31d36f23a9d51460d87e94f6600ee77ecc1e4de
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-09 20:00:52 +0530

    condition added

--- p2.c
+++ p2.c
@@ -9,5 +9,22 @@ int main()
     printf("Enter a number : ");
     scanf("%d", &n);
 
-     
+    for ( i = 0; i <= n; i++) //runs a loop that generates fibonacci series
+    { 
+        if (n==a || n==b || n==c) //checks if n is equal to any number in fibonacci series
+        {
+            printf("Entered number is in Fibonacci series.");
+            i=-1;
+            break;
+        }
+        
+        c=a+b; 
+        a=b;
+        b=c;
+    }
+    
+    if (i!=-1)  //will only print if there wasn't a match found in above for loop
+    {
+        printf("Entered number is not in Fibonacci series.");
+    }  
 }
