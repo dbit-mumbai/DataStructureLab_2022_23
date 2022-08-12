7bb3bdee26516f719bca0ed43723c66a00fb5638
dhaneshwaritendle <tendledhaneshwari@gmail.com>
2022-08-12 12:56:47 +0530

    added C-prog files

---
+++ p5-Largest.c
@@ -0,0 +1,30 @@
+//Name:Vanessa Jency 
+//Roll.no.37   Batch:B   Department:IT 
+//C program to find largest element in array
+
+
+#include <stdio.h>
+
+int main() {
+  int n;
+  double arr[100];
+  
+  printf("Enter the number of elements (1 to 100): ");
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; ++i) {
+    printf("Enter number%d: ", i + 1);
+    scanf("%lf", &arr[i]);
+  }
+
+  // storing the largest number to arr[0]
+  for (int i = 1; i < n; ++i) {
+    if (arr[0] < arr[i]) {
+      arr[0] = arr[i];
+    }
+  }
+
+  printf("Largest element = %.2lf", arr[0]);
+
+  return 0;
+}
