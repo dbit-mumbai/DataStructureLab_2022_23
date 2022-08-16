3a1529b9c7eabef7e8f7fd920f1ea0367dbed4e4
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-08-16 09:48:17 -0700

    Added Desending Order Program

---
+++ Decending Order of marks.c
@@ -0,0 +1,46 @@
+// NAME :- SUMIT SAWANT ROLL NO:- 51
+// 	Prog To Print Marks In decending Order 
+#include<stdio.h>
+
+struct Details {
+	int roll;
+	float marks;
+	char name[20];
+}s1[5],temp;
+
+int main()
+{	
+	int i,j;
+	// Taking Input
+	
+	for(i=0;i<5;i++)
+	{
+		printf("Enter Students %d Roll No: ",i+1);
+		scanf("%d",&s1[i].roll);
+		printf("Enter Students %d Name: ",i+1);
+		scanf("%s",&s1[i].name);
+		printf("Enter Students %d Marks: ",i+1);
+		scanf("%f",&s1[i].marks);
+	}
+		
+	// Sorting	
+	
+	for(i=0;i<5;i++)
+	{
+		for(j=i;j<5;j++)
+		{
+			if(s1[i].marks>s1[j].marks)
+			{
+				temp=s1[j];
+				s1[j]=s1[i];
+				s1[i]=temp;
+			}
+		}
+	}
+	printf("*************************************************\n Output:\n");
+	printf("Name \tRoll No\tMarks\n");	// Printing Output
+	for(i=0;i<5;i++)
+	{
+		printf("%s\t %d\t %f\n",s1[i].name,s1[i].roll,s1[i].marks);		
+	}
+}
