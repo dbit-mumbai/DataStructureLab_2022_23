29a5675a2a2dc1c01a3185c550b3965b259c485f
321dhruuv0037 <321dhruuv0037@dbit.in>
2022-08-16 20:22:32 +0530

    Initial commit

---
+++ p10.c
@@ -0,0 +1,56 @@
+/*  Author: Dhruuv Naik   Branch: IT  Roll no.: 39
+    
+    Write a menu driven program to perform the following operations in a Stack to 
+    1.Push    2.Pop   3.Stack empty?  4.Stack full    5.Clear stack   6.Show stack 
+*/
+
+#include <stdio.h>
+#define size 5 //setting stack size
+
+int stack[size], top=-1; //declaring array, top and functions
+void push();
+void pop();
+void empty();
+void full();
+void clear();
+void show();
+
+int main()
+{
+    int choice;
+
+    while(1) //to loop the program
+    {   //printing a menu
+        printf("\n*****Select a function to be performed*****");
+        printf("\n1.Push element \n2.Pop element \n3.Check if stack is empty \n4.Check if stack is full \n5.Clear stack \n6.Show stack \n7.Exit");
+        printf("\nEnter your choice : ");
+        scanf("%d", &choice); //saves the users choice
+ 
+        switch (choice)
+        {
+            case 1: push();
+                    break;
+        
+            case 2: pop();
+                    break;        
+            
+            case 3: empty();
+                    break;        
+            
+            case 4: full();
+                    break;        
+            
+            case 5: clear();
+                    break;
+    
+            case 6: show();
+                    break;
+
+            case 7: return 0; 
+        
+            default:printf("\nInvalid choice!\n");
+        }
+
+    }
+
+}
