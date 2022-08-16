479fddee82864367195511d5c8b157313d2e22ce
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-08-16 09:18:04 -0700

    Added Fibonacci Number Prog

---
+++ Fibonacci.c
@@ -0,0 +1,36 @@
+// Name :-  Sumit Sawant
+// Roll No:- 51
+// Prog To check if number is fibonacci Number
+int main(){
+	int n,i,j=10,f=0,s=1,t,count=0;
+	
+	// For Taking Input
+	printf("Enter The Number : ");
+	scanf("%d",&n);
+	
+	// Default Fibonacci number
+	if(n==0 || n==1)
+	{
+		printf("Number is a Fibonacci Number");
+	}
+	
+	while(t<n+1)
+	{
+		// For Calculating Fibonacci Number
+		t=f+s;
+		if(n==t){
+			printf("Number is a Fibonacci Number\n");
+			count=0;
+			break;
+
+		}
+		else  // For Initializing count to 1
+			count=1;
+		f=s;
+		s=t;
+	}
+	// For printing if number is not fibonacci
+	if(count==1){
+		printf("Number is not a fibonaci Number");
+	}
+}
