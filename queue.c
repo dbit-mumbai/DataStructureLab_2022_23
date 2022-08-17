9b95d3f1de38b58d0a6530df4299588b50f3666b
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-08-17 16:50:52 +0530

    Add files via upload

--- queue.c
+++ queue.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<stdlib.h>
-#define size 6
+#inlcude<stdlib.h>
 
 int queue[size];
-int i= -1;
-int j= -1;
+int i = -1;
+int j = -1;
 
-void enqueue(int);
+void enqueue(int n);
 void dequeue();
 void isEmpty();
 void show();
@@ -16,40 +15,117 @@ int main()
 	int ch, n;
 	while(1)
 	{
-		printf("\n\n Queue menu");
-		printf("\n\n enqueue");
-		printf("\n\n dequeue");
-		printf("\n\n Check empty");
-		printf("\n\n Display Queue");
-		printf("\n\n Exit");
-		printf("\n\n Enter your choice");
-		
+		printf("\n -------Queue Menu--------");
+		printf("\n1. Enqueue");
+		printf("\n2. Dequeue");
+		printf("\n3. Check Empty");
+		printf("\n4. Display Queue");
+		printf("\n5. Exit");
+		printf("\nEnter your choice:");
+		scanf("%d" , &ch);
+
 		switch(ch)
 		{
-		case 1:
-			printf"Enter an integer value:";
-			scanf("%d", &n);
-			enqueue(n);
-			break;
+			case 1: printf("Enter a Integer Value:\n");
+				scanf("%d", &n);
+				enqueue(n);
+				break;
+		
+			case 2: dequeue();
+				break;
+
+			case 3: isEmpty();
+				break;
+
+			case 4: show();
+				break;
+	
+			case 5: exit(0)
+				break;
+		
+			default: printf("\n Invalid Choice!");
 			
-		case 2:
-			dequeue();
-			break;
-			
-		case 3:
-			isEmpty();
-			break;
-			
-		case 4:
-			show();
-			break;
-			
-		case 5:
-			exit(0);
-			break;
-			
-		default:
-			printf("\n\n Invlid Choice");
 		}
-	}		
-			
+	}
+ return 0;
+}
+void enqueue(int n) {
+	if(f == -1 && r == -1)
+	{
+		f++;
+		queue[++r] = n;
+	}
+	else
+	{
+		if(r == size -1)
+		{
+			printf("\n Queue is full");
+
+		}
+		else 
+		{
+			queue[++r] = n;
+		}
+	}
+}
+void dequeue(){
+	if(f == -1 && r == -1)
+	{
+		printf("\n Queue is Empty");
+	}
+	else 
+	{
+		if(f == r)
+		{
+			f = -1;
+			r = -1;
+		}
+		else
+		{
+			f++;
+		}
+
+	}
+void isEmpty(){
+	if(f == -1 && r == -1)
+	{
+		printf("Queue is Empty");
+	}
+	else
+	{
+		printf("Queue is not Empty");
+	}
+}
+
+void show(){
+	int i;
+	if(f == -1 && r == -1)
+	{
+		printf("Queue is Empty");
+	}
+	else 
+	{
+		printf("\n-----------");
+		for(i = f; i<= r; i++)
+		{
+			if(i == f && i == r)
+			{
+				printf("\n%d <- front and rear" , queue[i]);
+			}
+			else if(i == f)
+			{
+				printf("\n%d <- front" , queue[i]);
+			}
+                        else if(i == r)
+			{
+				printf("\n%d <- rear" , queue[i]);
+			}
+			else 
+			{
+				printf("\n%d" , queue[i]);
+			}
+
+		}
+		printf("\n--------------");
+	}
+}
