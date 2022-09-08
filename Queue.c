89d61242f84dc50c109c078ab7c99dfc019a282b
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-08 08:43:55 -0700

    Added All Futions

--- Queue.c
+++ Queue.c
@@ -1,3 +1,66 @@
+void enqueue(){
+	int x;
+    if(front ==-1 && rear ==-1){
+        front = rear = 0;
+        printf("Enter the number :");
+        scanf("%d",&x);
+        queue[rear]=x;
+        printf("You entered %d\n",queue[rear]);
+    }
+    else if(rear == size - 1 ){
+    	printf("Queue is Full\n");
+	}
+    
+    else {
+    	rear = rear + 1;
+    	printf("Enter the number :");
+        scanf("%d",&x);
+        queue[rear]= x;
+        printf("You entered %d\n",queue[rear]);
+    }
+}
+void Dequeue(){
+	if((front==-1 && rear == -1) || front == rear + 1 ){
+	
+		front=rear=-1;
+		printf("Queue Is Empty\n");}
+	else
+	{
+		printf("The Element You Deleted is %d\n",queue[front]);
+		front= front + 1;
+	}
+}
+
+void Show(){
+	int x=front;
+	if((front==-1 && rear == -1)||(front == rear && rear != 0 )){
+	
+		printf("Queue Is Empty\n");
+	}
+	else {
+	
+	while(x != rear + 1){
+		printf("%d",queue[x]);
+ 			if(x==front)
+				printf("<--- front");
+			if(x==rear)
+				printf("<--- Rear");
+				printf("\n");
+		
+		x= x + 1;
+	}
+	}
+}
+
+
+void Count(){
+	int count = 0,x = front;
+	while(x != rear+1){
+		count=count+1;
+		x=x+1;
+	}
+	printf("The Number Of Enteries Are %d\n",count);
+}
 void main(){
 
     int i=0,n;
