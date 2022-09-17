be19c570a98bf98b03065836c4f7890373caaef9
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-09-16 20:43:08 -0700

    Added Funtion to Delete From

--- List.c
+++ List.c
@@ -6,6 +6,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
+
+
+
 // To Create A Node
 struct node{
     int data; // To store Data
@@ -13,7 +16,23 @@ struct node{
 };
 struct node* head;
 
+// To Delete Element From End Of LinkList
+void DeleteE(struct node *head){
+	struct node *ptr;
+	ptr=head;
+	while(ptr->next!=NULL){
+		ptr=ptr->next;
+	}
+	ptr->next=NULL;
+	printf("SuccessFull!!!!!\n");
+	Dis();
+}
+// Delete An Element From Beginning Of Linked List
+struct node* DeleteB(struct node* head){
+	head=head->next;
+}
 
+// Insert An Element At a Certain Position
 void InsertP(struct node *head,int data,int posi)
 {
 	struct node *temp = (struct node*)malloc(sizeof(struct node));// Creating A Temporary Node
@@ -35,6 +54,8 @@ void InsertP(struct node *head,int data,int posi)
 	Dis();
 	
 }
+
+// Display The Link List
 void Dis(){
 	int c=0;
 	printf("The List is:\n");
@@ -47,6 +68,7 @@ void Dis(){
 		
 	}
 }
+
 // Insertion At The Beggining
 struct node * InsertB(struct node* head,int x){
 	struct node *ptr,*temp;
@@ -57,6 +79,7 @@ struct node * InsertB(struct node* head,int x){
 	head=temp;
 	return head;
 }
+
 // Insertion At The End Of The List
 void InsertE(struct node *head,int x){
 	struct node *ptr,*temp;
@@ -70,42 +93,67 @@ void InsertE(struct node *head,int x){
 	}
 	ptr->next=temp;
 }
+
+// Main Body Of Code
 int main(){
+	
 	head =(struct node *)malloc(sizeof(struct node));
 	head=NULL;
+	
     int x,ch=1,b,p;
-    while(ch!=0){
-    printf("********* Link List Operations ***********\n");
-    printf("1) Insert From Beginning\n2) Insert From End ");
-    printf("\n3) Display the List\n4) Insert At Certain Position\n5)Exit");
-    printf("\nEnter Your Choice:");
-    scanf("%d",&x);
-    printf("\n\n\n");
-    switch(x){
-       		case 1: printf("Enter The NUmber:");
+    
+    while(ch!=0)
+	{
+    	printf("********* Link List Operations ***********\n\n");
+    	printf("1) Insert From Beginning\n2) Insert From End ");
+    	printf("\n3) Display the List\n4) Insert At Certain Position\n5) Delete From Beginning\n");
+    	printf("6) Exit");
+    	printf("\nEnter Your Choice:");
+    	scanf("%d",&x);
+    
+		printf("\n\n\n");
+    
+		switch(x)
+		{
+       		case 1: 
+			   		printf("Enter The NUmber:");
 					scanf("%d",&b);
-			   		head=InsertB(head,b);
+			   		
+					head=InsertB(head,b);
 					Dis(); 
        	  	 	    break;
        		case 2:
 					printf("Enter The NUmber:");
 					scanf("%d",&b);
-			   		InsertE(head,b);
+			   		
+					InsertE(head,b);
 			   		Dis();
-         	  	     break;
-        	case 3:Dis();
-          	 	     break;
-        	case 5: ch=0;
+         	  	    break;
+        	case 3: 
+					Dis();
+          	 	    break;
+        	case 6: 
+					ch=0;
             	    break;
         	case 4: 
         			printf("Enter The Position:");
         			scanf("%d",&p);
+					
 					printf("Enter The NUmber:");
 					scanf("%d",&b);
-			   		InsertP(head,b,p);
+			   		
+					InsertP(head,b,p);
 			   		Dis();
 			   		break;
-        	default: printf("You Entered a Wrong Choice!!!!");
+			case 5: 
+					head=DeleteB(head);
+					Dis();
+					break;
+			case 7:
+					DeleteE(head);
+					break;
+        	
+			default: printf("You Entered a Wrong Choice!!!!");
     	}
     }
 }
