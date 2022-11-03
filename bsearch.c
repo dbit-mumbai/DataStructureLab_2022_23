932334823faf644d99245b42fad2a7f1decaaa60
Yashas Khot <109984055+yashaskhot@users.noreply.github.com>
2022-11-04 00:34:03 +0530

    Update bsearch.c

--- bsearch.c
+++ bsearch.c
@@ -1,42 +1,169 @@
-// C program to implement iterative Binary Search
-#include <stdio.h>
+# include <stdio.h>
+#include <stdlib.h>
+// #include <conio.h>
 
-// A iterative binary search function. It returns
-// location of x in given array arr[l..r] if present,
-// otherwise -1
-int binarySearch(int arr[], int l, int r, int x)
+
+struct bt
 {
-	while (l <= r) {
-		int m = l + (r - l) / 2;
+int info;
+struct bt *left,*right;
+}node;
 
-		// Check if x is present at mid
-		if (arr[m] == x)
-			return m;
+struct bt *root=NULL;
 
-		// If x greater, ignore left half
-		if (arr[m] < x)
-			l = m + 1;
-
-		// If x is smaller, ignore right half
-		else
-			r = m - 1;
-	}
-
-	// if we reach here, then element was
-	// not present
-	return -1;
+struct bt* getnode (void)
+{
+return  (( struct bt* ) malloc (sizeof(struct bt)));
 }
 
-int main(void)
+void freenode(struct bt *p)
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = 10;
-	int result = binarySearch(arr, 0, n - 1, x);
-	(result == -1) ? printf("Element is not present"
-							" in array")
-				: printf("Element is present at "
-							"index %d",
-							result);
-	return 0;
+free(p);
+}
+
+struct bt* insert(struct bt *r,int x)
+{
+struct bt *newnode;
+
+
+   if(x==r->info)
+	printf("\n Duplicate Values.");
+   else if(x<r->info)
+    {
+       if(r->left==NULL)
+		{
+		  newnode=getnode();
+		  newnode->info=x;
+	  	  newnode->left=NULL;
+	      newnode->right= NULL;
+
+		  r->left=newnode;
+		}
+	   else
+	    insert(r->left,x);
+    }
+   else if(x>r->info)
+    {
+	if(r->right==NULL)
+		{
+		  newnode=getnode();
+		  newnode->info=x;
+	      newnode->left=NULL;
+	      newnode->right= NULL;
+
+		  r->right=newnode;
+		}
+	else
+	     insert(r->right,x);
+    }
+}
+
+
+void search(struct bt *r,int x)
+{
+ int found=0;
+ struct bt *temp;
+ temp=root;
+    while(temp!=NULL)
+    {
+      if(temp->info==x)
+	 {
+	   printf("\n The %d Element is Present",temp->info);
+	   found=1;
+	   break;
+	 }
+      if(temp->info>x)
+	 temp=temp->left;
+      else
+	 temp=temp->right;
+    }
+
+if(found==0)
+     printf("\nElement not found");
+}
+
+
+void inorder(struct bt *r)
+{
+ if(r!=NULL)
+  {
+    inorder(r->left);
+    printf("\n\t %d ",r->info);
+    inorder(r->right);
+  }
+}
+
+void preorder(struct bt *r)
+{
+ if(r!=NULL)
+  {
+
+    printf("\n\t %d ",r->info);
+    preorder(r->left);
+    preorder(r->right);
+  }
+}
+
+void postorder(struct bt *r)
+{
+ if(r!=NULL)
+  {
+    postorder(r->left);
+    postorder(r->right);
+    printf("\n\t %d ",r->info);
+  }
+}
+
+
+void main ()
+{
+int ch,x;
+struct bt *newnode;
+
+while(1)
+{
+
+printf("\n  ----- Linked List -----");
+printf("\n 1. Insert");
+printf("\n 2. PreOrder");
+printf("\n 3. InOrder");
+printf("\n 4. PostOrder");
+printf("\n 5. Search");
+printf("\n 6. Exit");
+
+printf("\n Enter your Choice : ");
+scanf("%d",&ch);
+switch(ch)
+	{
+	 case 1: printf(" Enter the node value:");
+		 scanf("\n %d",&x);
+		// count++;
+
+		 if(root==NULL)
+		 {
+		  newnode=getnode();
+		  newnode->info=x;
+		  newnode->left=NULL;
+		  newnode->right=NULL;
+		  root=newnode;
+		 }
+		 else
+		   insert(root,x);
+
+		 break;
+
+	case 2 : preorder(root); break;
+	case 3 : inorder(root); break;
+	case 4 : postorder(root); break;
+	case 5 : printf(" Enter the node value:");
+		 scanf("\n %d",&x);
+		 search(root,x);
+		 break;
+	case 6 : exit(1);
+	default : printf ("\n Wrong Choice");
+	}
+  
+}  // end of while
+
+
 }
