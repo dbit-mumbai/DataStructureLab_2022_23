a415ca281da3a5e0538cf0d5eed5d062793306cf
Vanessa1410 <110601743+Vanessa1410@users.noreply.github.com>
2022-11-13 04:17:22 +0530

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -1,47 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void insertIth(int a[], int n, int i)
-{
-    int key = a[i];
-    int j = i-1;
-    while(j >= 0 && a[j] > key) {
-        a[j+1] = a[j];
-        j = j - 1;
-    }
-    a[j+1] = key;
+int a[25], n;
+
+void insertion_sort(){
+    int i, j, temp, count = 0;
+    for(i=1;i<n;i++)
+	{
+		temp=a[i];
+		j=i-1;
+
+		while((temp<a[j])) //to shift the greater value towards right
+        {
+			a[j+1]=a[j]; 
+			j=j-1;
+		} 	
+		a[j+1]=temp; //to store the smaller digit(a[i]) at left
+        
+        count++;
+        printf("\nAfter %d pass : \n", count);
+        for ( temp = 0; temp < n; temp++)
+        {        
+            printf("%d\t", a[temp]);
+        }   
+    }    
 }
 
-void display(int a[], int n) 
-{
-    int i = 0;
-    while(i < n)
+int main(){
+    int i;
+    printf("\nEnter no of elements to be sorted : ");
+    scanf("%d", &n);
+    for ( i = 0; i < n; i++)
     {
-        printf("%d ", a[i]);
-        i++;
+        printf("Enter element %d : ", i+1);
+        scanf("%d", &a[i]);
     }
-    printf("\n");
-}
 
-void insertSort(int a[], int n)
-{
-    int i = 1;
-    while(i < n) 
+    insertion_sort();
+
+    printf("\nThe sorted list is : \n");
+    for ( i = 0; i < n; i++)
     {
-        insertIth(a, n, i);
-        i++;
+        printf("%d\t", a[i]);
     }
-}
-
-
-
-int main() 
-{
-    int arr[] = {1,5,2,6,2,1,7};
-    int size = sizeof(arr)/sizeof(int);
-    printf("Before Sort : ");
-    display(arr, size);
-    insertSort(arr, size);
-    printf("After Sort : ");
-    display(arr, size);
-    return 0;
 }
